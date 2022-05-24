19604f1b768c64638965a67fa14703e7c3d70ed9
yudizshiv <shivprajapat232@gmail.com>
2022-05-24 10:30:02 +0530

    Register page Setp Add complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/shared/Navigation/Navigation';
-import Home from './pages/Home/Home';
+import { Home, Register, Login } from './pages';
 
 function App() {
   return (
@@ -8,7 +8,10 @@ function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
---
+++ frontend/src/pages/Steps/index.js
@@ -0,0 +1,5 @@
+export { default as StepPhoneEmail } from './StepPhoneEmail/StepPhoneEmail'
+export { default as StepOtp } from './StepOtp/StepOtp'
+export { default as StepName } from './StepName/StepName'
+export { default as StepAvatar } from './StepAvatar/StepAvatar'
+export { default as StepUsername } from './StepUsername/StepUsername'
