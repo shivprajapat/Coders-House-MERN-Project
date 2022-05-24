c616e700c339e370647f72175c71fde43cf74400
yudizshiv <shivprajapat232@gmail.com>
2022-05-24 09:43:38 +0530

    Navigation Created

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { logo } from '../../../Assets/images'
-const Navigation = () => {
-    return (
-        <nav className='navbar container'>
-            <Link className="brandStyle" to="/">
-                <img src={logo} alt="logo" />
-                <span className="logo-txt">Coders house</span>
-            </Link>
+import { logo } from '../../../Assets/images';
+import styles from './navigation.module.css';
 
+const Navigation = () => {
+    const brandStyle = {
+        color: '#fff',
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        fontSize: '22px',
+        display: 'flex',
+        alignItems: 'center',
+    };
+
+    const logoText = {
+        marginLeft: '10px',
+    };
+      return (
+        <nav className={`${styles.navbar} container`}>
+            <Link style={brandStyle} to="/">
+                <img src={logo} alt="logo" />
+                <span style={logoText}>Codershouse</span>
+            </Link>
         </nav>
     );
 };
