3626ba1bf29d19b4d3c739d56e658d863752d019
shivprajapat <shivprajapat232@gmail.com>
2022-05-25 09:58:19 +0530

    Login and Register Folder Deletes

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,3 @@
 export { default as Home } from './Home/Home'
-export { default as Register } from './Register/Register'
-export { default as Login } from './Login/Login'
 export { default as Authenticate } from './Authenticate/Authenticate'
 export { default as Activate } from './Activate/Activate'
